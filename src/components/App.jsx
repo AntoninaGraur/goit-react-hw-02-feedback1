74213d098f0b6f2c138da2fdf87f6f078e2976e8
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-05-24 16:36:20 +0200

    not write

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+import {
+  FeedbackContainer,
+  FeedbackTitle,
+  FeedbackPositiveBtn,
+  FeedbackNeutralBtn,
+  FeedbackNegativeBtn,
+  FeedbackBtnContainer,
+  StatisticTitle,
+  StatisticText
+} from './Feedback/feedbackStatistic.styled';
+
+
+
+export class App extends Component {
+  state = {
+    feedbackCount: {
+      good: 0,
+      neutral: 0,
+      bad: 0
+    }
+  };
+
+  handleFeedback = (type) => {
+    this.setState((prevState) => ({
+      feedbackCount: {
+        ...prevState.feedbackCount,
+        [type]: prevState.feedbackCount[type] + 1
+      }
+    }));
+  };
+
+  render() {
+    const { feedbackCount } = this.state;
+
+    return (
+      <div>
+        <FeedbackContainer>
+          <FeedbackTitle>Please leave a feedback</FeedbackTitle>
+          <FeedbackBtnContainer>
+            <FeedbackPositiveBtn onClick={() => this.handleFeedback('good')} id="positiveBtn" type="submit">
+              Positive
+            </FeedbackPositiveBtn>
+            <FeedbackNeutralBtn onClick={() => this.handleFeedback('neutral')} id="neutralBtn" type="submit">
+              Neutral
+            </FeedbackNeutralBtn>
+            <FeedbackNegativeBtn onClick={() => this.handleFeedback('bad')} id="negativeBtn" type="submit">
+              Negative
+            </FeedbackNegativeBtn>
+          </FeedbackBtnContainer>
+        </FeedbackContainer>
+        <div>
+          <StatisticTitle>Statistic</StatisticTitle>
+          <StatisticText>Good: {feedbackCount.good}</StatisticText>
+          <StatisticText>Neutral: {feedbackCount.neutral}</StatisticText>
+          <StatisticText>Bad: {feedbackCount.bad}</StatisticText>
+        </div>
+      </div>
+    );
+  }
+}
+
+
